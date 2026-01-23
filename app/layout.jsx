57e642efdc510d1b4fb855bce4f8e052001e97f8
imHermes1 <lukeforn@gmail.com>
2026-01-23 18:45:21 +1100

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -78,10 +78,12 @@ export const metadata = {
   manifest: '/site.webmanifest',
   icons: {
     icon: [
-      { url: '/favicon.jpg', type: 'image/jpeg' },
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
     apple: [
-      { url: '/favicon.jpg', type: 'image/jpeg' },
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
   },
 };
